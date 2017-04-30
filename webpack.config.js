8828c057722990ccc76e7f6b92ae8c287a037536
Rabin Gaire <rabingaire20@gmail.com>
2017-04-30 14:13:55 +0545

    added configuration ready for reactjs

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src',
+    output: {
+        path : __dirname,
+        filename: 'bundle.js'
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'es2015', 'stage-0', 'react'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css')
+    ],
+};
