36d396eb0c2ad520d680bc6782e8f0be9ef5cf12
Rabin Gaire <rabingaire20@gmail.com>
2017-04-30 17:48:43 +0545

    form now works fine

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { Form, Button } from './common';
-
 export default class Main extends React.Component {
     constructor(props) {
         super(props);
@@ -10,13 +8,20 @@ export default class Main extends React.Component {
         };
     }
 
+    onSubmit() {
+        let text = this.refs.inputForm.value;
+        this.setState({text});
+    }
+
     render(){
         return (
             <div>
                 <div className="row mgtp-10">
-                    <div className="small-12 medium-4 medium-centered columns">
-                        <Form type="text" placeholder="Enter Text"/>
-                        <Button value="Post"/>
+                    <div className="small-12 medium-4 medium-centered columns text-center">
+                        <form>
+                            <input type="text" placeholder="Enter Text..." ref="inputForm"/>
+                            <a href="#" className="button" onClick={this.onSubmit.bind(this)}>Post</a>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/components/common/index.js
+++
@@ -1,2 +0,0 @@
-export Form from './Form';
-export Button from './Button';
